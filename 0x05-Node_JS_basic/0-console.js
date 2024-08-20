19cb65a50e423390638e03839b27ae47cd58cdc4
Mikasa12345667 <gmodera6@gmail.comclearclear>
2024-08-20 14:58:16 -0700

    update

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Gabriel Odera <https://github.com/Mikasa12345667>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
